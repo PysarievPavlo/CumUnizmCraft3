0e20ff46bca8831166ef91bdca3568e8e4c2e642
PysarievPavlo <91278442+PysarievPavlo@users.noreply.github.com>
2025-02-17 21:13:24 +0200

    Update config.ts

--- config.ts
+++ config.ts
@@ -11,8 +11,8 @@ export const window = {
 };
 
 export const api = {
-    ws: 'ws://127.0.0.1:1370/ws',
-    web: 'http://127.0.0.1:1370',
+    ws: 'ws://77.109.56.174:1370/ws',
+    web: 'http://77.109.56.174:1370',
     // extraToken: token,
 };
 
