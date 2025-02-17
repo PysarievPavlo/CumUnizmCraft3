3bbcb8ee4e5628bd5c8a29d7d69a6ad87e5ef856
PysarievPavlo <91278442+PysarievPavlo@users.noreply.github.com>
2025-02-17 22:53:56 +0200

    Update config.ts

--- config.ts
+++ config.ts
@@ -11,8 +11,8 @@ export const window = {
 };
 
 export const api = {
-    ws: 'ws://77.109.56.174:1370/ws',
-    web: 'http://77.109.56.174:1370',
+    ws: 'ws://cumunizmcraft.pp.ua:1370/ws',
+    web: 'http://cumunizmcraft.pp.ua:1370',
     publicKey,
 };
 
